c1391e1aaafa5e75ec50f619ac0705909b27ac48
Dev_Pelz <pelzfx1@gmail.com>
2023-09-28 21:32:47 +0100

    add: contracts/IVoting.sol

---
+++ contracts/IVoting.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IVoting {
+    function createBid(
+        address[] memory _voters,
+        string memory _name,
+        uint _voting_Duration
+    ) external;
+
+    function addVoter(address _newVoter, uint _bidID) external;
+
+    function voteYes(uint _bidID) external;
+
+    function voteNo(uint _bidID) external;
+
+    function getPositiveVoteCount(uint _bidID) external;
+
+    function getNegativeVoteCount(uint _bidID) external;
+
+    function bidResults(uint _bidID) external;
+
+    function revokeVotingRights(address _voter, uint _bidID) external;
+
+    function resetVoters(uint _bidID) external;
+
+    function changeOwner(address _newOwner) external;
+}
